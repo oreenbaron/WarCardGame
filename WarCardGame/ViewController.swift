1fb35e49371cab05f8a65d1e4ad30897539bb07d
Oreen Baron <orinbaron@icloud.com>
2018-04-25 12:35:07 +0300

    Initial Commit

---
+++ WarCardGame/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WarCardGame
+//
+//  Created by Orin Baron on 4/25/18.
+//  Copyright © 2018 Oreen Baron. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
